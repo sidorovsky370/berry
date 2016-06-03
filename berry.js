98915a1624dd35e493fd1955d37a1fe244ad99a4
Aleksei Sidorov <a.stakanchik@mail.ru>
2016-06-03 16:12:25 +0300

    berry.get стал асинхронным 2.0.7.1

--- berry.js
+++ berry.js
@@ -58,11 +58,15 @@
 	};
 
 	berry.isArray = function(arr) {
-		return (Object.prototype.toString.call(arr) === '[object Array]') ? true : false;
+		return Object.prototype.toString.call(arr) === '[object Array]'; // ;)
+	}
+
+	berry.isObject = function(obj) {
+		return Object.prototype.toString.call(obj) === '[object Object]'; // ;)
 	}
 
 	berry.isFunction = function(func) {
-		return (Object.prototype.toString.call(func) === '[object Function]') ? true : false;
+		return Object.prototype.toString.call(func) === '[object Function]'; // ;)
 	}
 	
 	berry.ready = function(func) {
@@ -167,7 +171,7 @@
 	/* парсим все аргументы, при необходимости заполняем дефолтными значениями. Далее передаем в функцию обновления */
 	berry.define = function(name, depents, callback, data) {
 		//Если первый полученный аргумент Объект и второй функция или не передан, то значит мы получили конфиг и обработаем его через специальную функцию.
-		if (typeof arguments[0] === "object") {
+		if (berry.isObject(arguments[0]) /* typeof value === 'object' пропустит null */) {
 			return berry._config(arguments[0], arguments[1]);
 		}
 		//Если первый аргумент не является строкой, то сообщаем об ошибке и возвращаем false
@@ -180,7 +184,7 @@
 			var config = {};
 
 			// парсим все входящие аргументы функции, определяем их тип и заполняем внутренний объект аргументов
-			for (var i = 0; i < arguments.length; i++) {
+			for (var i = 0, l = arguments.length; i < l; i++) {
 				var argument = arguments[i];
 
 				// если аргумент является строкой, то значит это название модуля (name)
@@ -193,7 +197,7 @@
 				else if ( berry.isFunction(argument) ) config.callback = argument;
 
 				// если аргумент является обьектом, то значит это обьект дополнительных данных модуля
-				else if (typeof argument === 'object' && !Array.isArray(argument)) config.data = argument;
+				else if ( berry.isObject(argument) /* typeof value === 'object' пропустит null */ ) config.data = argument;
 			}
 
 			// если аргументы не нашли, то поставим их значение по умолчанию равным false
@@ -236,8 +240,7 @@
 		// создаем хранилище переменных модуля
 		if( !module.storage ) module.storage = [];
 
-		//если в имени передан урл файла, сохраним урл
-		if (/\.(css|js)$/.test(config.name) && !module.data.path) module.data.path = config.name;
+
 		if (/\.(css)$/.test(module.data.path)) module.data.type = 'html';
 
 		//первоначальная инцииализация дефолтных значений
@@ -259,38 +262,54 @@
 			// если в были определены зависимости для модуля и параметр require не равен null, то проверим весь список зависимостей и загрузим их
 			if (module.depents.length > 0 && module.data.require !== null) {
 
-				// перерменная необходимости загрузки
 				berry.stack = [module.name];
 				// проверим массив зависимостей, если в нем есть значения (0, null, false), то такой модуль не будем загружать
 				/*
 					По логике мы можем передать в массиве зависимостей не только имя модуля, от который зависит текущий модуль,
 					но и булевые значения или число элементов DOM, test по регулярному выражения - любое проверочное уравнение, которое возвращает true\false.
 				*/
-				var check = true;
-				module.depents.forEach(function(depent) {
-					if (!depent) check = false;
+
+				var check = !module.depents.some(function(depent) {
+					// если существует хотя бы одна зависимость,
+                    // которая обращается в false, то выходим из цикла,
+                    // функция some при этом вернет true;
+                    // forEach же продолжил бы цикл до последнего элемента;
+					return !depent;
 				});
-				
+
 				// если перерменная необходимости загрузки все еще true, то модуль надо вызвать
 				if (check === true) {
 
 					//Запустим обновление модулей рекурсивно проверяя зависимости
-					var response = false;
 					module.depents.forEach(function(depent) {
 						if (typeof depent === 'string') {
 							//добавим зависимость в массив
 							berry.stack.push( berry.defined[depent] );
-							response = berry._call( berry.defined[depent] );
+
+							berry._call( berry.defined[depent] ).then(function(module){
+                                // модуль загружен;
+                                //
+                                berry.stack.pop();
+                                if (berry.stack.length === 1 && berry.stack === module.name) {
+                                    // все зависимости разрешены!!!
+                                    // нужно решить, что далее с этим делать;)
+                                }
+                            }, function(module){
+                                // нет необходимости грузить модуль;
+                            });
 						}
-						else {
-							response = true;
-						}					
 					});
-					
-					if (response && module.data.require === true) {
-						berry.get(module);
-						resolve(module);
-					}
+
+					if (module.data.require === true) {
+						berry.get(module).then(function(module){
+                            resolve(module);
+                        }, function(error, module){
+                           // ошибка при загрузке модуля;
+                           reject(module);
+                        });
+					} else {
+                        resolve(module);
+                    }
 				}
 
 				// если перерменная необходимости загрузки равна false, то значит нет необходимости грузить модуль
@@ -298,14 +317,18 @@
 					// сбросим параметр require в null, это необходимо? если данный модуль будет вызван позже и пройдет проверку необходимости загрузки
 					module.data.require = null;
 					reject(module);
-				}			
+				}
 			}
 
-			// если параметр require не равен null, тогда обновим параметры модуль используя обьект аргументов
+			/ параметр require не равен null, тогда обновим параметры модуль используя обьект аргументов
 			else {
-				berry.get(module);
-				resolve(module);
-			}			
+				berry.get(module).then(function(module){
+                   resolve(module);
+                }, function(error, module)r
+                   // ошибка при загрузке модуля;
+                   reject(module);
+                });
+			}
 		});
 	}
 
@@ -364,32 +387,39 @@
 	berry.get = function(module, url) {
 		console.log('GET', module);
 		
-		// URL модуля
-		var url = url || module.data.path || false;
+        var self = this;
+        return new Promise(function(resolve, reject){
+            // URL модуля
+            var url = url || module.data.path || false;
 
-		// Если url модуля есть, то будем его подгружать
-		if (url) {
-			this._xhr(url).then(function(response) {
-				if (berry.config.debug) console.info('Модуль ' + module.name + ' загружен', response);
+            // Если url модуля есть, то будем его подгружать
+            if (url) {
+                self._xhr(url).then(function(response) {
+                    if (berry.config.debug) console.info('Модуль ' + module.name + ' загружен', response);
 
-				module.response = response;
-				
-				// передадим исполнение callback-функции в спец метод
-				berry._callback(module);
-				
-				return true;
-			}, function(error) {
-				console.error("Ошибка!", error);
-				return false;
-			});
-		}
+                    module.response = response;
 
-		// Если url нет
-		else {
-			// передадим исполнение callback-функции в спец метод
-			this._callback(module);
-			return true;
-		}
+                    // передадим исполнение callback-функции в спец метод
+                    berry._callback(module);
+
+                    // модуль загружен - разрешен;
+                    resolve(module);
+                }, function(error) {
+                    console.error("Ошибка!", error);
+                    // модуль не загружен
+                    reject(error, module);
+                });
+            }
+
+            // Если url нет
+            else {
+                // передадим исполнение callback-функции в спец метод
+                self._callback(module);
+
+                // модуль разрешен;
+                resolve(module);
+            }
+        });
 	}
 	
 	// AMD. Загрузка библиотеки	по url
